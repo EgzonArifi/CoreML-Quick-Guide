94fb9c8d72b25f644e8ba5c5b0e5e5e1650ed9c9
Egzon's Macbook <egzonarifi@live.com>
2017-09-04 20:18:53 +0200

    initial commit

---
+++ CoreML-Demo/InceptionController.swift
@@ -0,0 +1,18 @@
+//
+//  InceptionController.swift
+//  CoreML-Demo
+//
+//  Created by EgzonArifi on 9/4/17.
+//  Copyright © 2017 Overjump. All rights reserved.
+//
+
+import UIKit
+
+class InceptionController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
